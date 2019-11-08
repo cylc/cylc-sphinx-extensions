2ba8148a0b739b83cc5b58c7a87b6dafe9548801
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2019-11-08 18:10:08 +0000

    cylc_lang: merge cylc_domain

--- cylc/sphinx_ext/cylc_lang/__init__.py
+++ cylc/sphinx_ext/cylc_lang/__init__.py
@@ -59,12 +59,75 @@ Pygments language: `cylc-graph`
    @wall_clock => foo => bar
    (foo & bar) => pub
 
+Cylc Sphinx Domain
+
+.. code-block:: rst
+
+   .. cylc:conf:: my-conf.rc
+
+      .. cylc:setting:: foo
+
+         a setting called ``foo``.
+
+        see also :cylc:setting:`[bar]baz` (this is a relative reference)
+
+      .. cylc:section:: bar
+
+         a section called ``bar``.
+
+         .. cylc:setting:: baz
+
+            a setting called ``baz``
+
+            .. cylc:value:: integer
+
+               :deprecated: 1.2.3
+
+               seconds as an integer.
+
+               the newer :cylc:value:`string` is prefered (this is also
+               a relative reference).
+
+            .. cylc:value:: string
+
+               an iso8601 duration.
+
+.. cylc:conf:: my-conf.rc
+
+  .. cylc:setting:: foo
+
+     a setting called ``foo``.
+
+    see also :cylc:setting:`[bar]baz` (this is a relative reference)
+
+  .. cylc:section:: bar
+
+     a section called ``bar``.
+
+     .. cylc:setting:: baz
+
+        a setting called ``baz``
+
+        .. cylc:value:: integer
+
+           :deprecated: 1.2.3
+
+           seconds as an integer.
+
+           the newer :cylc:value:`string` is prefered (this is also
+           a relative reference).
+
+        .. cylc:value:: string
+
+           an iso8601 duration.
+
 '''
 
 from cylc.sphinx_ext.cylc_lang.lexers import CylcLexer, CylcGraphLexer
+from cylc.sphinx_ext.cylc_lang.domains import CylcDomain
 
 
-__all__ = ['CylcLexer', 'CylcGraphLexer', 'setup']
+__all__ = ['CylcLexer', 'CylcGraphLexer', 'CylcDomain', 'setup']
 
 __version__ = '1.0.0'
 
@@ -73,4 +136,5 @@ def setup(app):
     """Sphinx plugin setup function."""
     app.add_lexer('cylc', CylcLexer())
     app.add_lexer('cylc-graph', CylcGraphLexer())
+    app.add_domain(CylcDomain)
     return {'version': __version__, 'parallel_read_safe': True}
