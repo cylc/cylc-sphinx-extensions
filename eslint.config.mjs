5d090de636dc5aa59b6d235e6b4257ca68cd28ed
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2024-08-01 09:19:42 +0100

    f

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,7 @@
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [...compat.extends("eslint:recommended"), {
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.jquery,
-        },
-
-        ecmaVersion: 6,
-        sourceType: "script",
-    },
-}];
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+);
